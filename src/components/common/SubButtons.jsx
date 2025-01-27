2b485971a3792f9c51fb011df1e502d9fca585bc
abbas056 <abbassharaliat32@gmail.com>
2025-01-27 14:04:36 +0500

    updates

--- src/components/common/SubButtons.jsx
+++ src/components/common/SubButtons.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function SubButtons({ subBtn1name, subBtn2name, subTabs, setSubTabs }) {
+  // States
   const tabSwitch = (id) => {
     let newCat = {
       [subBtn1name]: false,
